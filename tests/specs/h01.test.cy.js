1580a3f2d9b800d6960c1ea5d3d076ae1d36fd4e
Dawid Draguła <dawid.dragula@gmail.com>
2024-07-23 11:13:17 +0200

    Added descriptions to expects.

--- tests/specs/h01.test.cy.js
+++ tests/specs/h01.test.cy.js
@@ -8,14 +8,14 @@ describe('01-simple-column', () => {
       const chart = Highcharts.charts[0];
       const dataMax = chart.yAxis[0].dataMax;
 
-      expect(chart.series).to.have.length(3);
+      expect(chart.series, "There should be 3 series").to.have.length(3);
 
       for (const series of chart.series) {
         for (const point of series.points) {
           if (point.y === dataMax) {
-            expect(point.dataLabel.text.textStr).to.equal('max');
+            expect(point.dataLabel.text.textStr, "Max value points should have data label with text 'max'").to.equal('max');
           } else if (point.dataLabel) {
-            expect(point.dataLabel.text.textStr).to.equal('');
+            expect(point.dataLabel.text.textStr, "Other points should not have data labels").to.equal('');
           }
         }
       }
@@ -27,7 +27,7 @@ describe('01-simple-column', () => {
       const chart = Highcharts.charts[0];
       const dataMax = chart.yAxis[0].dataMax;
 
-      expect(chart.yAxis[0].max).to.equal(dataMax * 2);
+      expect(chart.yAxis[0].max, "Y-Axis max should be exactly 2 times the max value").to.equal(dataMax * 2);
     });
   });
 
@@ -36,8 +36,15 @@ describe('01-simple-column', () => {
       const chart = Highcharts.charts[0];
       const dataMax = chart.yAxis[0].dataMax;
 
-      expect(chart.yAxis[0].plotLinesAndBands[0].options.value).to.equal(dataMax * 1.5);
-      expect(chart.yAxis[0].plotLinesAndBands[0].options.dashStyle).to.equal('dash');
+      expect(
+        chart.yAxis[0].plotLinesAndBands[0].options.value,
+        "Plot line should be at 1.5 times the max value"
+      ).to.equal(dataMax * 1.5);
+
+      expect(
+        chart.yAxis[0].plotLinesAndBands[0].options.dashStyle,
+        "Plot line should be dashed"
+      ).to.equal('dash');
     });
   });
 });
