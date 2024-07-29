e2f8df7188c6d6222f53b9a568dabdeadb5d04c3
K009 <spidahas@gmail.com>
2024-07-29 12:16:51 +0200

    Added tests for the 09-visible-points-zoom exercise.

--- tests/specs/h09.test.cy.js
+++ tests/specs/h09.test.cy.js
@@ -1,5 +1,103 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Line Chart Exercise", () => {
+  beforeEach("passes", () => {
+    cy.visit("../../exercises/09-visible-points-zoom/index.html");
+  });
+
+  it('should check if the chart has 100 random integer values and "xy" zoom', () => {
+    cy.window()
+      .its("Highcharts")
+      .then((Highcharts) => {
+        const chart = Highcharts.charts[0];
+        const series = chart.series[0];
+
+        expect(series.data.length, "Series should have 100 points").to.equal(
+          100
+        );
+        expect(
+          chart.options.chart.zoomType,
+          "Zoom type should be 'xy'"
+        ).to.equal("xy");
+      });
+  });
+
+  it("should check if the visible points label updates correctly on zoom", () => {
+    cy.window()
+      .its("Highcharts")
+      .then((Highcharts) => {
+        const chart = Highcharts.charts[0];
+
+        function checkVisiblePointsLabel(expectedCount) {
+          const customLabelText = chart.customLabel.textStr;
+          expect(
+            customLabelText,
+            `Visible points label should update correctly`
+          ).to.equal(`Visible points: ${expectedCount}`);
+        }
+
+        checkVisiblePointsLabel(100);
+
+        chart.xAxis[0].setExtremes(45, 50);
+
+        checkVisiblePointsLabel(
+          chart.series[0].points.filter((point) => point.isInside).length
+        );
+      });
+  });
+
+  it("should check if highest value points have red labels and red dots on xAxis", () => {
+    cy.window()
+      .its("Highcharts")
+      .then((Highcharts) => {
+        const chart = Highcharts.charts[0];
+        const series = chart.series[0];
+        const dataMax = Math.max(...series.yData);
+        const maxPoints = series.points.filter((point) => point.y === dataMax);
+
+        const customMaxPointLabels = chart.maxPointLabels;
+        const customXAxisPoints = chart.xAxisPoints;
+
+        // Check that the number of labels and dots matches the number of max points
+        expect(
+          customMaxPointLabels.length,
+          "The number of customMaxPointLabels should match the number of max points"
+        ).to.equal(maxPoints.length);
+
+        expect(
+          customXAxisPoints.length,
+          "The number of customXAxisPoints should match the number of max points"
+        ).to.equal(maxPoints.length);
+
+        maxPoints.forEach((point) => {
+          const matchingCustomLabels = customMaxPointLabels.filter(
+            (customMaxPointLabel) =>
+              customMaxPointLabel.coordinates.x === point.x &&
+              customMaxPointLabel.coordinates.y === point.y
+          );
+
+          const matchingCustomDots = customXAxisPoints.filter(
+            (customXAxisPoint) =>
+              customXAxisPoint.coordinates.x === point.x &&
+              customXAxisPoint.coordinates.y === point.y
+          );
+
+          // Check that the correct label displays the maximum y value
+          matchingCustomLabels.forEach((matchingCustomLabel) => {
+            const label = matchingCustomLabel.label;
+            expect(
+              label.textStr,
+              `Max point label placed on the ${matchingCustomLabel.coordinates.x} x position should display the maximum y value`
+            ).to.equal(point.y);
+          });
+
+          // Check that the custom red dot is placed on the correct x position
+          matchingCustomDots.forEach((matchingCustomDot) => {
+            const dotCoordinates = matchingCustomDot.coordinates;
+            expect(
+              dotCoordinates.x,
+              `The custom red dot should be placed on the ${point.x} x position`
+            ).to.equal(point.x);
+          });
+        });
+      });
+  });
+});
